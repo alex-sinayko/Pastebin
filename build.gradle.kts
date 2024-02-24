22e565eb69dfa2f9f73fa32c3ffa2b730d82b575
alex <alex@sinayko.dev>
2024-02-24 14:18:51 +0200

    Init

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version "3.2.2"
+    id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "dev.sinayko"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    compileOnly ("org.projectlombok:lombok")
+    runtimeOnly ("org.postgresql:postgresql")
+    annotationProcessor ("org.projectlombok:lombok")
+
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
